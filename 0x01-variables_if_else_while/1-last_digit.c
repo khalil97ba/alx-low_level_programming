fe07f2c3ce783cb29a93a4cc315ddd23bf23924e
ibrahima ba <iba83219@gmail.com>
2023-02-17 14:51:22 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,8 +6,8 @@
  * main - Entry point
  * Description: Program assigns a random number to n each time it is executed.
  * It then prints the last digit of the number stored in the variable n.
- * Return: Always 0 
- * Author: ibrahima ba
+ * Return: Always 0
+ * Author: Ibrahima ba
  */
 
 int main(void)
@@ -33,7 +33,5 @@ if
 {
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
 }
-
 return	(0);
-
 }
