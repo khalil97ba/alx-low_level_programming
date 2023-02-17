d8e185a96c4e248dd74b9a4221435a24607758fd
ibrahima ba <iba83219@gmail.com>
2023-02-17 15:52:26 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print numbers 0 to 9 followed by a newline
+ * Return: Always 0
+ * Author: ibrahima ba
+ */
+
+int main(void) {
+int i;
+  for ( i = 0; i < 10; i++)
+{
+    printf("%d\n", i);
+  }
+  return 0;
+}
