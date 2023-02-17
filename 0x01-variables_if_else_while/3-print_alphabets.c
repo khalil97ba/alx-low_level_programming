27968072d6ae80ad614104781a593f3b636e9fcf
ibrahima ba <iba83219@gmail.com>
2023-02-17 15:24:11 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: Print lower and upper alphabet on the same line
+ * Author: Ibrahima ba
+ */
+
+int main(void)
+{
+char z = 'a';
+char Z = 'A';
+
+while (z <= 'z')
+{
+	putchar(z);
+	z++;
+}
+while (Z <= 'Z')
+{
+	putchar(Z);
+	Z++;
+}
+putchar('\n');
+return (0);
+}
