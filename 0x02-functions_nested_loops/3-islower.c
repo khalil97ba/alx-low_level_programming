76ce48c0cd3e1ab458e9f0935bb68c5067629595
ibrahima ba <iba83219@gmail.com>
2023-02-21 08:29:16 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _islower - Entry point
+  * Description: Checks for lowercase character.
+  * @c: Contains value to be compared
+  * Return: Always 0
+  */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
