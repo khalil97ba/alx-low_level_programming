0cada52a53f9ceba061f7fef8074945eae326f69
ibrahima ba <iba83219@gmail.com>
2023-02-21 05:51:19 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,21 @@
 /**
  * main - Entry point
  * Description: prints _putchar, followed by a new line.
- * Return: Always 0
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
  */
 
 int main(void)
 {
-	char n = "_putchar";
-	int i;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	for (i = 0; i < n; i++)
-	{
-		_putchar('n');
-		_putchar('\n');
-	}
 return (0);
 }
