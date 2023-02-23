7de936048a48c3a81db528b64117f7239b3d2812
ibrahima ba <iba83219@gmail.com>
2023-02-23 05:00:33 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,18 +4,22 @@
  *
  * Return: Always 0.
  */
+
 void print_most_numbers(void)
 {
-	int i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i + '0')
-		}
-	}
+int i;
 
-	_putchar('\n');
+i = 0;
+while (i < 10)
+{
+if (i != 2 && i != 4)
+{
+_putchar (i + '0');
+}
+i++;
+}
+
+_putchar ('\n');
 }
 
