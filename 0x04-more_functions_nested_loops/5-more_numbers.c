06e27878d30cb912d332e1d1dbc44c0e147bca0b
ibrahima ba <iba83219@gmail.com>
2023-02-23 07:33:42 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,20 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 14; i++)
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 0; j < 10; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i + '0');
-		}
-	}
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+			}
 
-	_putchar('\n');
+			_putchar((i % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
 }
+
+
