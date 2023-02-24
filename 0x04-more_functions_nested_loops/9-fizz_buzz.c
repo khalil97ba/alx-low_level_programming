f70bf3cc31a83d2b53d3baa02b160bee71f1ccb2
ibrahima ba <iba83219@gmail.com>
2023-02-24 08:27:34 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,33 +7,40 @@
 
 int main(void)
 {
-	int i;
 
-	for (i = 0; i <= 100; i++)
-	{
-		if (i % 15 == 0)
-		{
-			printf("FizzBuzz");
-		}
+int i;
 
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-
-		else
-		{
-			printf("%i", i);
-		}
-	}
-
-	printf("\n");
-	return (0);
+i = 1;
+while (i <= 100)
+{
+if (i % 15)
+{
+printf("FizzBuzz ");
 }
 
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
 
+else if (i % 5 == 0)
+{
+if (i < 100)
+printf("Buzz ");
+
+else
+printf("Buzz");
+}
+
+else
+{
+printf("%i ", i);
+}
+
+i++;
+
+
+}
+printf("\n");
+return (0);
+}
