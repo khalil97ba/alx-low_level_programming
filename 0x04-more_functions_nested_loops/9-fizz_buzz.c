b19fb3bd86fb806c34f0458bba609b923e325419
ibrahima ba <iba83219@gmail.com>
2023-02-24 05:15:43 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,8 +18,17 @@ int main(void)
 
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			if (i < 100)
+			{
+				printf("Buzz");
+			}
+
+			else
+			{
+				printf("Buzz");
+			}
 		}
+
 
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
